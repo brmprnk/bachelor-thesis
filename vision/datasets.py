0d3fd29bc427e45c320e951367a23530d66112f0
Bram Pronk <I.B.Pronk@student.tudelft.nl>
2021-05-18 22:24:24 +0200

    Update datasets to only process 80 percent for prediction calculation

--- vision/datasets.py
+++ vision/datasets.py
@@ -4,9 +4,11 @@ from __future__ import absolute_import
 
 import numpy as np
 import pandas as pd
+import math
 from torch.utils.data.dataset import Dataset
 
-N_MODALITIES = 6
+N_MODALITIES = 2
+TRAINING_DATA_SPLIT = 0.8
 VALID_PARTITIONS = {'train': 0, 'val': 1, 'test': 2}
 
 
@@ -22,23 +24,32 @@ class TCGADataset(Dataset):
         print("Getting {} Dataset".format(self.split))
         split_per = 0.1
 
-        rna_file = "/Users/bram/rp-group-21-bpronk/data/TCGA/RNA_COMMONGNC.csv"
+        # RNA-seq
+        rna_file = "/Users/bram/Desktop/CSE3000/data/RNA_COMMONGNC.csv"
         rna_data = pd.read_csv(rna_file, usecols=range(1, 5001))
         print("-----   RNA file read   -----")
+
+        # Use first 80% of samples for training
+        num_training_samples = math.ceil(TRAINING_DATA_SPLIT * rna_data.shape[0])
+        rna_data = rna_data.head(num_training_samples)
         print("RNA SHAPE : ", rna_data.shape)
 
+        # RNA splitting for validation and training loss
         rna_test_split = rna_data.sample(frac=split_per)
-
         self.rna_test_file = np.nan_to_num(np.float32(rna_test_split.to_numpy()))
         self.rna_train_file = np.nan_to_num(np.float32(rna_data.to_numpy()))
 
-        gcn_file = "/Users/bram/rp-group-21-bpronk/data/TCGA/GNC_POSITIVE_COMMONRNA.csv"
+        # Gene Copy Number
+        gcn_file = "/Users/bram/Desktop/CSE3000/data/GNC_POSITIVE_COMMONRNA.csv"
         gcn_data = pd.read_csv(gcn_file, usecols=range(1, 5001))
         print("-----   GCN file read   -----")
+
+        # Use first 80% of samples for training
+        num_training_samples = math.ceil(TRAINING_DATA_SPLIT * gcn_data.shape[0])
+        gcn_data = gcn_data.head(num_training_samples)
         print("GCN SHAPE : ", gcn_data.shape)
 
         gcn_test_split = gcn_data.sample(frac=split_per)
-
         self.gcn_test_file = np.nan_to_num(np.float32(gcn_test_split.to_numpy()))
         self.gcn_train_file = np.nan_to_num(np.float32(gcn_data.to_numpy()))
 
