67da73894387bebcc91b1e09766777301b671648
Bram Pronk <I.B.Pronk@student.tudelft.nl>
2021-05-27 16:15:33 +0200

    Rewrite prediction file, cleaner and add logging

--- vision/predict.py
+++ vision/predict.py
@@ -1,16 +1,8 @@
-from __future__ import division
-from __future__ import print_function
-from __future__ import absolute_import
-
-import numpy as np
-import pandas as pd
-
 import torch
-import math
+
 from sklearn.metrics import mean_squared_error
 from train import load_checkpoint
-
-PREDICT_DATA_SPLIT = 0.2
+import datasets
 
 
 def predict_loss(recon_data, input_data):
@@ -21,52 +13,74 @@ def predict_loss(recon_data, input_data):
     @param input_data:
     @return: int MSE
     """
-    recon_data = recon_data.detach().numpy()
-    input_data = input_data.values
-
     return mean_squared_error(input_data, recon_data)
 
 
 if __name__ == "__main__":
 
     print("-----   Loading Trained Model   -----")
-    model_path = "/Users/bram/multimodal-vae-public-master/vision/trained_models/80RNA_GCN.pth.tar"
+    model_path = "/Users/bram/multimodal-vae-public-master/vision/results/PoE 26-05-2021 08:20:17/with_prior.pth.tar"
     model = load_checkpoint(model_path, use_cuda=False)
     model.eval()
 
-    print("-----   RNA file read   -----")
-    rna_file = "/Users/bram/Desktop/CSE3000/data/RNA_COMMONGNC.csv"
-    rna_data = pd.read_csv(rna_file, usecols=range(1, 5001))
-    num_predict_samples = math.ceil(PREDICT_DATA_SPLIT * rna_data.shape[0])
-    rna_data_predict_split = rna_data.tail(num_predict_samples)
-    print("RNA SHAPE : ", rna_data_predict_split.shape)
+    indices_path = "/Users/bram/multimodal-vae-public-master/vision/results/PoE 26-05-2021 08:20:17"
+    tcga_data = datasets.TCGAData(indices_path=indices_path)
 
-    print("-----   GCN file read   -----")
-    gcn_file = "/Users/bram/Desktop/CSE3000/data/GNC_POSITIVE_COMMONRNA.csv"
-    gcn_data = pd.read_csv(gcn_file, usecols=range(1, 5001))
-    num_predict_samples = math.ceil(PREDICT_DATA_SPLIT * gcn_data.shape[0])
-    gcn_data_predict_split = gcn_data.tail(num_predict_samples)
-    print("GCN SHAPE : ", gcn_data_predict_split.shape)
+    predict_dataset = tcga_data.get_data_partition("predict")
 
-    rna_sample = np.nan_to_num(np.float32(rna_data_predict_split.to_numpy()))
-    gcn_sample = np.nan_to_num(np.float32(gcn_data_predict_split.to_numpy()))
+    # 1 batch for prediction
+    predict_loader = torch.utils.data.DataLoader(predict_dataset, batch_size=len(predict_dataset), shuffle=False)
 
-    print(model)
-    rna_sample = torch.Tensor(rna_sample)
-    gcn_sample = torch.Tensor(gcn_sample)
+    # FOR 2 Predictions
+    for batch_idx, (rna, gcn, dna) in enumerate(predict_loader):
+        (joint_recon_rna, joint_recon_gcn, joint_recon_dna, joint_mu, joint_logvar) = model(dna=dna)
 
-    (rna_recon_rna, rna_recon_gcn, rna_mu, rna_logvar) = model(rna=rna_sample)
-    (gcn_recon_rna, gcn_recon_gcn, gcn_mu, gcn_logvar) = model(gcn=gcn_sample)
+        predicted_rna = joint_recon_rna.detach().numpy()
 
-    # # generate image and text
-    # SIGMOID
-    # image_recon = F.sigmoid(model.image_decoder(sample)).cpu().data
-    # gray_recon = F.sigmoid(model.gray_decoder(sample)).cpu().data
+    for batch_idx, (rna, gcn, dna) in enumerate(predict_loader):
+        (joint_recon_rna, joint_recon_gcn, joint_recon_dna, joint_mu, joint_logvar) = model(dna=dna)
 
-    rna_prediction_loss = predict_loss(gcn_recon_rna, rna_data_predict_split)
-    gcn_prediction_loss = predict_loss(rna_recon_gcn, gcn_data_predict_split)
+        predicted_gcn = joint_recon_gcn.detach().numpy()
 
-    print("Prediction loss for RNA given GCN = ", rna_prediction_loss)
-    print("Prediction loss for GCN given RNA = ", gcn_prediction_loss)
+    for batch_idx, (rna, gcn, dna) in enumerate(predict_loader):
+        (joint_recon_rna, joint_recon_gcn, joint_recon_dna, joint_mu, joint_logvar) = model(gcn=gcn)
 
-    print("Total prediction loss = ", (rna_prediction_loss + gcn_prediction_loss) / 2)
+        predicted_dna = joint_recon_dna.detach().numpy()
+
+    rna_loss = predict_loss(predicted_rna, predict_dataset.rna_data)
+    gcn_loss = predict_loss(predicted_gcn, predict_dataset.gcn_data)
+    dna_loss = predict_loss(predicted_dna, predict_dataset.dna_data)
+
+    print("rna loss = ", rna_loss)
+    print("gcn loss = ", gcn_loss)
+    print("dna loss = ", dna_loss)
+
+    # Output results
+    result_dir = "/Users/bram/multimodal-vae-public-master/vision/predict_results"
+    # Current Time from model for output files
+    dt_string = "PoE 25-05-2021 17:12:45"
+
+    # file3 = open("{}/3.txt".format(result_dir), "a")
+    # file3.write("RNA Joint at {} ||| {}\n".format(dt_string, rna_loss))
+    # file3.write("GCN Joint at {} ||| {}\n".format(dt_string, gcn_loss))
+    # file3.write("DNA Joint at {} ||| {}\n\n".format(dt_string, dna_loss))
+    # file3.close()
+    #
+    # file2 = open("{}/2.txt".format(result_dir), "a")
+    # file2.write("RNA from others at {} ||| {}\n".format(dt_string, rna_loss))
+    # file2.write("GCN from others at {} ||| {}\n".format(dt_string, gcn_loss))
+    # file2.write("DNA from others at {} ||| {}\n\n".format(dt_string, dna_loss))
+    # file2.close()
+    #
+    file1 = open("{}/1.txt".format(result_dir), "a")
+    # file1.write("RNA from GCN at {} ||| {}\n".format(dt_string, rna_loss))
+    file1.write("RNA from DNA at {} ||| {}\n".format(dt_string, rna_loss))
+    # file1.write("GCN from RNA at {} ||| {}\n".format(dt_string, gcn_loss))
+    file1.write("GCN from DNA at {} ||| {}\n".format(dt_string, gcn_loss))
+    # file1.write("DNA from RNA at {} ||| {}\n".format(dt_string, dna_loss))
+    file1.write("DNA from GCN at {} ||| {}\n".format(dt_string, dna_loss))
+
+    # file1.write("RNA from RNA (unimodal) at {} ||| {}\n".format(dt_string, rna_loss))
+    # file1.write("GCN from GCN (unimodal) at {} ||| {}\n".format(dt_string, gcn_loss))
+    # file1.write("DNA from DNA (unimodal) at {} ||| {}\n".format(dt_string, dna_loss))
+    file1.close()
