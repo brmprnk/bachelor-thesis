ca8fe77f7f72a824307bc2a18aafbe6dd73105f6
Bram Pronk <I.B.Pronk@student.tudelft.nl>
2021-05-31 15:03:41 +0200

    Retrieve prediction results from one modality

--- vision/predict.py
+++ vision/predict.py
@@ -19,11 +19,11 @@ def predict_loss(recon_data, input_data):
 if __name__ == "__main__":
 
     print("-----   Loading Trained Model   -----")
-    model_path = "/Users/bram/multimodal-vae-public-master/vision/results/PoE 26-05-2021 08:20:17/with_prior.pth.tar"
+    model_path = "/Users/bram/multimodal-vae-public-master/vision/results/three_modal_first_results/best_model.pth.tar"
     model = load_checkpoint(model_path, use_cuda=False)
     model.eval()
 
-    indices_path = "/Users/bram/multimodal-vae-public-master/vision/results/PoE 26-05-2021 08:20:17"
+    indices_path = "/Users/bram/multimodal-vae-public-master/vision/results/three_modal_first_results"
     tcga_data = datasets.TCGAData(indices_path=indices_path)
 
     predict_dataset = tcga_data.get_data_partition("predict")
@@ -33,32 +33,38 @@ if __name__ == "__main__":
 
     # FOR 2 Predictions
     for batch_idx, (rna, gcn, dna) in enumerate(predict_loader):
-        (joint_recon_rna, joint_recon_gcn, joint_recon_dna, joint_mu, joint_logvar) = model(dna=dna)
+        (rna_recon_rna, rna_recon_gcn, rna_recon_dna, joint_mu, joint_logvar) = model(rna=rna)
 
-        predicted_rna = joint_recon_rna.detach().numpy()
-
-    for batch_idx, (rna, gcn, dna) in enumerate(predict_loader):
-        (joint_recon_rna, joint_recon_gcn, joint_recon_dna, joint_mu, joint_logvar) = model(dna=dna)
-
-        predicted_gcn = joint_recon_gcn.detach().numpy()
+        rna_predicted_rna = rna_recon_rna.detach().numpy()
+        rna_predicted_gcn = rna_recon_rna.detach().numpy()
+        rna_predicted_dna = rna_recon_rna.detach().numpy()
 
     for batch_idx, (rna, gcn, dna) in enumerate(predict_loader):
-        (joint_recon_rna, joint_recon_gcn, joint_recon_dna, joint_mu, joint_logvar) = model(gcn=gcn)
+        (gcn_recon_rna, gcn_recon_gcn, gcn_recon_dna, joint_mu, joint_logvar) = model(gcn=gcn)
 
-        predicted_dna = joint_recon_dna.detach().numpy()
+        gcn_predicted_rna = gcn_recon_rna.detach().numpy()
+        gcn_predicted_gcn = gcn_recon_rna.detach().numpy()
+        gcn_predicted_dna = gcn_recon_rna.detach().numpy()
 
-    rna_loss = predict_loss(predicted_rna, predict_dataset.rna_data)
-    gcn_loss = predict_loss(predicted_gcn, predict_dataset.gcn_data)
-    dna_loss = predict_loss(predicted_dna, predict_dataset.dna_data)
+    for batch_idx, (rna, gcn, dna) in enumerate(predict_loader):
+        (dna_recon_rna, dna_recon_gcn, dna_recon_dna, joint_mu, joint_logvar) = model(dna=dna)
 
-    print("rna loss = ", rna_loss)
-    print("gcn loss = ", gcn_loss)
-    print("dna loss = ", dna_loss)
+        dna_predicted_rna = dna_recon_rna.detach().numpy()
+        dna_predicted_gcn = dna_recon_gcn.detach().numpy()
+        dna_predicted_dna = dna_recon_dna.detach().numpy()
+
+    # rna_loss = predict_loss(predicted_rna, predict_dataset.rna_data)
+    # gcn_loss = predict_loss(predicted_gcn, predict_dataset.gcn_data)
+    # dna_loss = predict_loss(predicted_dna, predict_dataset.dna_data)
+
+    # print("rna loss = ", rna_loss)
+    # print("gcn loss = ", gcn_loss)
+    # print("dna loss = ", dna_loss)
 
     # Output results
-    result_dir = "/Users/bram/multimodal-vae-public-master/vision/predict_results"
+    result_dir = "/Users/bram/multimodal-vae-public-master/vision/results/three_modal_first_results"
     # Current Time from model for output files
-    dt_string = "PoE 25-05-2021 17:12:45"
+    dt_string = "temp"
 
     # file3 = open("{}/3.txt".format(result_dir), "a")
     # file3.write("RNA Joint at {} ||| {}\n".format(dt_string, rna_loss))
@@ -72,15 +78,15 @@ if __name__ == "__main__":
     # file2.write("DNA from others at {} ||| {}\n\n".format(dt_string, dna_loss))
     # file2.close()
     #
-    file1 = open("{}/1.txt".format(result_dir), "a")
-    # file1.write("RNA from GCN at {} ||| {}\n".format(dt_string, rna_loss))
-    file1.write("RNA from DNA at {} ||| {}\n".format(dt_string, rna_loss))
-    # file1.write("GCN from RNA at {} ||| {}\n".format(dt_string, gcn_loss))
-    file1.write("GCN from DNA at {} ||| {}\n".format(dt_string, gcn_loss))
-    # file1.write("DNA from RNA at {} ||| {}\n".format(dt_string, dna_loss))
-    file1.write("DNA from GCN at {} ||| {}\n".format(dt_string, dna_loss))
+    file1 = open("{}/predict_results.txt".format(result_dir), "a")
+    file1.write("GCN from RNA at {} ||| {}\n".format(dt_string, predict_loss(rna_predicted_gcn, predict_dataset.gcn_data)))
+    file1.write("DNA from RNA at {} ||| {}\n".format(dt_string, predict_loss(rna_predicted_dna, predict_dataset.dna_data)))
+    file1.write("RNA from GCN at {} ||| {}\n".format(dt_string, predict_loss(gcn_predicted_rna, predict_dataset.rna_data)))
+    file1.write("DNA from GCN at {} ||| {}\n".format(dt_string, predict_loss(gcn_predicted_dna, predict_dataset.dna_data)))
+    file1.write("RNA from DNA at {} ||| {}\n".format(dt_string, predict_loss(dna_predicted_rna, predict_dataset.rna_data)))
+    file1.write("GCN from DNA at {} ||| {}\n".format(dt_string, predict_loss(dna_predicted_gcn, predict_dataset.gcn_data)))
 
-    # file1.write("RNA from RNA (unimodal) at {} ||| {}\n".format(dt_string, rna_loss))
-    # file1.write("GCN from GCN (unimodal) at {} ||| {}\n".format(dt_string, gcn_loss))
-    # file1.write("DNA from DNA (unimodal) at {} ||| {}\n".format(dt_string, dna_loss))
+    file1.write("RNA from RNA (unimodal) at {} ||| {}\n".format(dt_string, predict_loss(rna_predicted_rna, predict_dataset.rna_data)))
+    file1.write("GCN from GCN (unimodal) at {} ||| {}\n".format(dt_string, predict_loss(gcn_predicted_gcn, predict_dataset.gcn_data)))
+    file1.write("DNA from DNA (unimodal) at {} ||| {}\n".format(dt_string, predict_loss(dna_predicted_dna, predict_dataset.dna_data)))
     file1.close()
